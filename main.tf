faaf2a5028a2a28893443c77e8f023425190b61c
Md Shamim <shamimice03@gmail.com>
2023-01-19 07:48:50 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 resource "aws_instance" "demo_server" {
   ami           = "ami-0bba69335379e17f8"
   instance_type = "t3.micro"
-  count = 3
 
-  tags = {
-    Name = "demo-server"
-    Team = "Dev-Team"
-  }
 }
