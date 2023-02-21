8056f6a4ec620327a85d781b765703e096064488
shamim <shamimice03@gmail.com>
2023-02-21 01:34:01 +0000

    main branch added

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 resource "aws_instance" "demo_server1" {
   ami           = "ami-0bba69335379e17f8"
   instance_type = "t3.micro"
+}
 
+resource "aws_instance" "demo_server2" {
+  ami           = "ami-0bba69335379e17f8"
+  instance_type = "t3.micro"
 }
